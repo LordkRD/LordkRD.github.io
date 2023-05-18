0e9cb3b9bde4ea5c6d325141c5cbcb0a17426d7e
Dinner Hernández <130079401+LordkRD@users.noreply.github.com>
2023-05-18 18:43:27 -0400

    se agregaron mejoras
    se agregaron mejoras al codigo

--- Encriptador.js
+++ Encriptador.js
@@ -4,17 +4,42 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
 */
+
 const codigos = ["enter", "imes", "ai", "ober", "ufat"];
 const vocales = ["e", "i", "a", "o", "u"];
 
+document.getElementById("textIn").addEventListener("input", normalizarTexto);
 
+// Esta función se encarga de asegurarse de que el texto esta normalizado (sin mayúsculas y caracteres especiales)
+function normalizarTexto() {
+
+	let textIn = document.getElementById("textIn").value;
+
+	if (textIn.value != "") {
+
+		let sinAcento = textIn.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+		let sinCaracteresEspeciales = sinAcento.normalize("NFD").replace(/[^a-zñ0-9. ]/g, "");
+		document.getElementById("textIn").value = sinCaracteresEspeciales;
+
+	}
+
+}
+
+document.getElementById("textIn").addEventListener("keypress", digitalLetras);
+// Esta función se encarga de asegurarse que el usuario sólo ingrese letras al digital y presionar teclas.
+function digitalLetras(e) {
+
+	if (e.key.match(/[a-zñ0-9 ]/g) === null) {
+		mostrarMensaje("validarMayuscula")
+		e.preventDefault();
+	}
+
+}
+// Esta función se encarga de encriptar un mensaje utilizando un algoritmo específico.
 function encriptar() {
 
-	document.getElementById("textIn").focus();
 	let textIn = document.getElementById("textIn").value;
-	let sinAcento = textIn.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // para quitar el acento
-	let sinCaracteres = sinAcento.replace(/[^a-z0-9 ]/g, ''); // omitir caracteres especiales
-	let textDividido = Array.from(sinAcento); // para convertir el texto en un array
+	let textDividido = Array.from(textIn); // para convertir el texto en un array
 
 	if (textIn == "") {
 		mostrarMensaje("textareaVacio");
@@ -28,23 +53,27 @@ function encriptar() {
 					document.getElementById("textOut").value = textEncriptado;
 					document.getElementById("textOut").style.backgroundImage = 'none';
 					break;
-				} else { document.getElementById("textOut").value = textDividido.join("") ; }
+				} else { document.getElementById("textOut").value = textDividido.join(""); }
 			}
 		}
 
-		document.getElementById("textOut").setAttribute("title","Texto Original: "+"'"+textIn+"'" );	
+		document.getElementById("textOut").setAttribute("title", "Texto Original: " + "'" + textIn + "'");
 		document.getElementById("textIn").value = "";
+
 	}
-	verificar();
+	
+
 }
 
+// Esta función se encarga de desencriptar un texto utilizando el algoritmo específico implementado. 
+// Es llamada cuando el botón 'botonDesencriptar' es presionado.
 function desencriptar() {
 	let textIn = document.getElementById("textIn").value;
 	if (textIn == "") {
 		mostrarMensaje("textareaVacio");
 	} else {
 
-		
+
 		let desencriptador = textIn.replace(/ai/gi, "a").replace(/enter/gi, "e").
 			replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
 
@@ -53,6 +82,8 @@ function desencriptar() {
 	}
 }
 
+// Esta función copia el contenido del elemento HTML con el ID 'textOut' al portapapeles del sistema operativo. 
+// Es llamada cuando el botón 'botonCopiar' es presionado.
 function copiarTexto(textOut) {
 
 	let textGuardado = document.createElement("textarea");
@@ -64,34 +95,31 @@ function copiarTexto(textOut) {
 		textGuardado.select();
 		document.execCommand("copy");
 		document.body.removeChild(textGuardado);
+
 		mostrarMensaje("textoCopiado");
 
-	}else{
-	mostrarMensaje("textoNoCopiado");
-}
-
-}
-
-function mostrarMensaje(mtext){
+	} else {
+		
+		mostrarMensaje("textoNoCopiado");
+	}
 	
+
+}
+
+
+
+// Esta función muestra un mensaje en una ventana emergente con el texto pasado como parámetro.
+function mostrarMensaje(mtext) {
+
 	let mensaje = document.getElementById(mtext);
 	mensaje.className = "show";
 	setTimeout(function () { mensaje.className = mensaje.className.replace("show", ""); }, 1000);
-	
+
 }
 
-document.getElementById("textIn").addEventListener("keypress",verificar);
-function verificar(e) {
- 
-	// comprovamos con una expresion regular que el caracter pulsado sea
-	// una letra, numero o un espacio
-	if(e.key.match(/[a-zñ]/g)===null) {
-		mostrarMensaje("validarMayuscula")
-		// Si la tecla pulsada no es la correcta, eliminado la pulsación
-		e.preventDefault();
-	}
-}
-
+// Este código obtiene los elementos de botón por su id y 
+// asigna manejadores de evento a ellos. Cuando cada botón es pulsado, 
+// llamará a la función correspondiente 'copiarTexto', 'encriptar' o 'desencriptar'.
 let botonCopiar = document.getElementById("botonCopiar");
 botonCopiar.onclick = copiarTexto;
 
